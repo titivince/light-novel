13ad128260a4544dd7e603ba994b79fda7b4e29d
titivince <72084738+titivince@users.noreply.github.com>
2020-12-23 14:52:11 +0100

    add README
    some optimisation
    cleaning code

---
+++ README.md
@@ -0,0 +1,24 @@
+### Light Novel
+You can use **everything** for your work
+One **simple** thing you have to do is put a [link](https://github.com/titivince/light-novel) to the original code or simply create a _fork_
+
+#### :warning: This work is under big progress :warning:
+
+#### State of the project pre-Alpha
+
+so feel free to help me in my work
+
+report all issue you can find
+
+##### Thing done :
+
+- [x] everything fully responsive
+- [x] fix infite loop **Major bug**
+- [x] creates and operates the following text
+- [x] Create an typing effect
+
+##### To do list :
+
+- [ ] add **css** for the character
+- [ ] add start/option screen
+- [ ] fix possible issue
--- script.js
+++ script.js
@@ -2,61 +2,57 @@ window.onload = function() {
 var outName = document.getElementById("name");
 var outTxt = document.getElementById("txt");
 var suivant = document.getElementById("suivant");
-var speed = 40; /* speed of the effetc in ms */
+var speed = 30; /* speed of the effect in ms */
 var i = 0;
-var n = 1;
 var currentText = 0; /* counting var */
-var newText = 1;    /* counting var */
 var mc = {
   name: "Vous",
-  txt: "Salut ! Je vien d'arriver dans cette nouvelle ville , et bien sur une nouvelle école et de nouveaux amis.",
+  txt: "Salut ! Je vien d'arriver dans cette nouvelle ville , et bien sur une nouvelle école et de nouveaux amis. Clicker sur la flèche pour passer au texte suivant.",
 };
 /* create function */
 function fastWriter() { /* create typing effect */
-  if(mc.name.length > mc.txt.length){ /* test whos longer length */
+  if(mc.name.length > mc.txt.length){ /* test who is longer */
     if (i < mc.name.length) {
-      outName.innerHTML += mc.name.charAt(i);
-      outTxt.innerHTML += mc.txt.charAt(i);
-      i++;
-      setTimeout( fastWriter, speed);
+      outName.innerHTML += mc.name.charAt(i); /* place the character at the end */
+      outTxt.innerHTML += mc.txt.charAt(i); /* place the character at the end */
     }
   }else{
     if (i < mc.txt.length) {
-      outName.innerHTML += mc.name.charAt(i);
-      outTxt.innerHTML += mc.txt.charAt(i);
-      i++;
-      setTimeout( fastWriter, speed);
+      outName.innerHTML += mc.name.charAt(i); /* place the character at the end */
+      outTxt.innerHTML += mc.txt.charAt(i); /* place the character at the end */
     }
   }
+  i++;
+  setTimeout( fastWriter, speed);
 }
 function control() {
   console.log("current text :", currentText); /* Keep track in the story */
-  if (newText == currentText) {
-    newText++;
-  }
 }
-function nextText() {
-  n = 1;
-  if ( currentText == n ) {
-    mc.txt = "J'ai changé de ville car mon père a été promus dans l'entreprise.";
+function nextText() { /* create the main function */
+  if ( currentText == 1 ) {
+    mc.txt = "J'ai changé de ville car mon père a été promus dans l'entreprise. Pourquoi je parle de \"clicker\" sur une flèche à moi même ?";
   }
-  n++;
-  if (currentText == n ) {
-    mc.txt = "J'ai changé d'école aussi alors je suis seul dans cette nouvelle ville et perdue en plus";
+  if ( currentText == 2 ) {
+    mc.txt = "J'ai changé d'école aussi alors je suis seul dans cette nouvelle ville. Je vien de me perdre acause de ce \"clicker sur la flèche pour passer au texte suivant\".";
   }
+  if ( currentText == 3 ) {
+    mc.txt = "Bref je n'ai cas demander a un passant mon chemin.";
+  }
+  /* for more text just add if  */
 }
 function forward() {
   i = 0;
+  currentText++;
   control();
   nextText();
-  currentText++;
   outName.innerHTML = ''; /* clear the name in the HTML */
   outTxt.innerHTML = ''; /* clear the text in the HTML */
   fastWriter();
 }
 /* start code */
 suivant.addEventListener("click", forward);
-
+console.log("here you can see were you are in the story");
+control();
 fastWriter();
 
 }
